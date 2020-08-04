c993459fc6815886f7f76617a6ae7bf5cbcee372
agnynureza <rezaagny@gmail.com>
2020-08-05 01:18:55 +0700

    pekan 3 : tugas 12

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Tugas11 from './tugas11/tugas11';
+import Tugas12 from './tugas12/tugas12';
 
 function App() {
   return (
     <div>
       <Tugas11/>
+      <Tugas12 start = {5} />
     </div>
   );
 }
---
+++ src/tugas12/tugas12.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Moment from 'moment'
+import './tugas12.css'
+
+class Tugas12 extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            time: 0,
+            timeNow : Moment().format('hh:mm:ss A')
+        }
+    }
+    componentDidUpdate(){
+        if(this.state.time <= 0){
+            this.componentWillUnmount()
+        }
+    }
+    componentDidMount(){
+        if(this.props.start !== undefined){
+            this.setState({time: this.props.start})
+        }
+
+        this.timerID = setInterval(
+            ()=> this.tick(), 1000
+        )
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID)
+    }
+
+    tick(){
+        this.setState({
+            time: this.state.time - 1
+        })
+    }
+
+  render(){
+    return(
+      <>
+      { this.state.time > 0 &&
+        <div>
+          <h2>
+            Sekarang jam : {this.state.timeNow}
+          </h2>
+          <h2>
+            Hitung Mundur : {this.state.time}
+          </h2>
+        </div>
+      }
+      </>
+    )
+  }
+}
+ 
+export default Tugas12;
