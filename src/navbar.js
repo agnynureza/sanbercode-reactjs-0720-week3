b7b7ee5409730a0ae15d1ca004f7bdde941314ba
agnynureza <rezaagny@gmail.com>
2020-08-08 06:03:11 +0700

    pekan 3 : tugas 15

--- src/navbar.js
+++ src/navbar.js
@@ -4,27 +4,24 @@ import {Link} from "react-router-dom";
 const Nav = () => {
   return (
     <>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<nav style = {{backgroundColor:"#563d7c"}} class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
 			<a class="navbar-brand" href="#">React JS Bootcamp</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
 			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="#"><Link to="/">Tugas 11</Link></a>
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<Link class="nav-link" to="/">Tugas 11</Link>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#"><Link to="/tugas12">Tugas 12</Link></a>
+						<Link class="nav-link" to="/tugas12">Tugas 12</Link>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#"><Link to="/tugas13">Tugas 13</Link></a>
+						<Link class="nav-link" to="/tugas13">Tugas 13</Link>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#"><Link to="/tugas14">Tugas 14</Link></a>
+						<Link class="nav-link" to="/tugas14">Tugas 14</Link>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">	<Link to="/tugas15">Tugas 15</Link></a>
+						<Link class="nav-link"  to="/tugas15">Tugas 15</Link>
 					</li>
 				</ul>
 			</div>
